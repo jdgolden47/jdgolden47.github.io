8cedc9b6388753ee691c27662c06a6718ce8b2db
jdgolden47 <134396092+jdgolden47@users.noreply.github.com>
2023-06-01 13:46:32 -0400

    upload

---
+++ .next/static/e3cWLuHa50hMDDWCOiteh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/745-4ff900c3cd97f88b.js","static/css/c66d02d86e111836.css","static/chunks/pages/index-59c050aecbe34d43.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
